3bdcd5fce0ea7c8e883b82a50702a05d307a4b0a
aidejuewang <aidejuewang@gmail.com>
2014-03-30 21:56:34 +0800

    xiaomi display

---
+++ common.mk
@@ -0,0 +1,39 @@
+#Common headers
+common_includes := hardware/qcom/display-legacy/libgralloc
+common_includes += hardware/qcom/display-legacy/libgenlock
+common_includes += hardware/qcom/display-legacy/liboverlay
+common_includes += hardware/qcom/display-legacy/libcopybit
+common_includes += hardware/qcom/display-legacy/libqdutils
+common_includes += hardware/qcom/display-legacy/libhwcomposer
+
+ifeq ($(TARGET_USES_POST_PROCESSING),true)
+    common_flags     += -DUSES_POST_PROCESSING
+    common_includes += $(TARGET_OUT_HEADERS)/pp/inc
+endif
+
+
+#Common libraries external to display HAL
+common_libs := liblog libutils libcutils libhardware
+
+#Common C flags
+common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers
+
+ifeq ($(ARCH_ARM_HAVE_NEON),true)
+    common_flags += -D__ARM_HAVE_NEON
+endif
+
+ifeq ($(TARGET_NO_HW_VSYNC),true)
+    common_flags += -DNO_HW_VSYNC
+endif
+
+ifeq ($(TARGET_USES_QCOM_BSP),true)
+    common_flags += -DQCOM_BSP
+endif
+
+common_deps :=
+kernel_includes :=
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+endif
